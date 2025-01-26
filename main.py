dbb8473ca09357048f8c0d6b4ec3483795e006d8
chris <christianarevalo184@gmail.com>
2025-01-25 23:49:54 -0500

    Remove redundant setting specification (already done within bot subclass)

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import asyncio
 import asyncpg
 import os
-import discord
 from bot.aio_crypto import AIOCrypto
 from dotenv import load_dotenv
 from pathlib import Path
@@ -9,14 +8,10 @@ from pathlib import Path
 dotenv_path = Path('AIOCrypto/.env')
 load_dotenv(dotenv_path=dotenv_path)
 
-
 async def main():
     db_name = os.getenv('DATABASE_NAME')
     db_username = os.getenv('DATABASE_USERNAME')
     db_password = os.getenv('DATABASE_PASSWORD')
-    intents = discord.Intents.default()
-    intents.message_content = True
-    intents.members = True
 
     async with asyncpg.create_pool(user=db_username, database=db_name, password=db_password) as pool:
         async with AIOCrypto() as bot:
