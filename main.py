8bc524b2de49170e01f57c62663caa96622bfb66
itsroar19 <christianarevalo184@gmail.com>
2025-02-01 21:29:37 -0500

    Switch from .env to YAML config support and made create_pool cleaner by using dsn directly

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 import asyncio
 import asyncpg
-import os
 from bot.aio_crypto import AIOCrypto
-from dotenv import load_dotenv
-from pathlib import Path
-
-dotenv_path = Path('AIOCrypto/.env')
-load_dotenv(dotenv_path=dotenv_path)
+from config.config import load_config
 
 async def main():
-    db_name = os.getenv('DATABASE_NAME')
-    db_username = os.getenv('DATABASE_USERNAME')
-    db_password = os.getenv('DATABASE_PASSWORD')
 
-    async with asyncpg.create_pool(user=db_username, database=db_name, password=db_password) as pool:
-        async with AIOCrypto() as bot:
-            bot.db = pool
+    config = load_config()
+    database_url = config['database_url']
+
+    async with asyncpg.create_pool(dsn=database_url) as pool:
+        async with AIOCrypto(db_pool=pool, config=config) as bot:
             await bot.run()
 
 if __name__ == '__main__':
